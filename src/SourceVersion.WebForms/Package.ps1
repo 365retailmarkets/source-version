2f9c3f7d57b1c95b45fc05acca5568b715cc2cea
Jeff <jeffrey.pierson@gmail.com>
2016-10-27 23:41:39 -0400

    Add comment to Publish.ps1 on how to push package

--- src/SourceVersion.WebForms/Package.ps1
+++ src/SourceVersion.WebForms/Package.ps1
@@ -1,3 +1,4 @@
 ﻿mkdir bin/packages/ -ErrorAction Ignore
 nuget pack .\SourceVersion.WebForms.csproj -outputdirectory bin/packages
 nuget init bin/packages/ c:\deleteme\nuget-packages
+#nuget push SourceVersion.WebForms.1.0.0.41879.nupkg $env:NUGETAPIKEY -Source https://www.nuget.org/api/v2/package
