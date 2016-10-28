6f3043fa1412271923cc47701f7171a8e12b75e7
Jeff <jeffrey.pierson@gmail.com>
2016-10-27 23:12:38 -0400

    Correct project name references

--- src/SourceVersion.WebForms/Package.ps1
+++ src/SourceVersion.WebForms/Package.ps1
@@ -1,3 +1,3 @@
 ﻿mkdir bin/packages/ -ErrorAction Ignore
-nuget pack .\SourceVersion.csproj -outputdirectory bin/packages
+nuget pack .\SourceVersion.WebForms.csproj -outputdirectory bin/packages
 nuget init bin/packages/ c:\deleteme\nuget-packages
