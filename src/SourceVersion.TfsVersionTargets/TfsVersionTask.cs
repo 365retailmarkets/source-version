0371a5ee25fabc2efb17ef0a737b6d56c87098e3
Jeff <jeffrey.pierson@gmail.com>
2017-03-09 16:06:13 -0500

    Support VersionFormat and AssemblyInformationalFormat

--- src/SourceVersion.TfsVersionTargets/TfsVersionTask.cs
+++ src/SourceVersion.TfsVersionTargets/TfsVersionTask.cs
@@ -18,6 +18,16 @@ namespace SourceVersion.TfsVersionTargets
         [Required]
         public string IntermediateOutputPath { get; set; }
 
+        /// <summary>
+        /// Specifies the version prefix to use to override the default value
+        /// </summary>
+        public string VersionFormat { get; set; }
+
+        /// <summary>
+        /// Specifies the suffix to use for the AssemblyInformationalVersion attribute.
+        /// </summary>
+        public string AssemblyInformationalFormat { get; set; }
+
         [Output]
         public string AssemblyInfoTempFilePath { get; set; }
 
@@ -55,7 +65,13 @@ namespace SourceVersion.TfsVersionTargets
             //if (watermark)
             {
                 this.Log.LogMessage(MessageImportance.High, "Generating AssemblyInfo content");
-                var generatedAssemblyInfoText = GenerateAssemblyInfo(watermark.MostRecentChangeset.Value);
+
+                string versionFormat = VersionFormat ?? "1.0.0.{changeset}";
+                string assemblyInformationalFormat = 
+                    AssemblyInformationalFormat ?? 
+                    "ServerPath.{server-path}.Changeset.{changeset}.{clean}";
+
+                var generatedAssemblyInfoText = GenerateAssemblyInfo(watermark, versionFormat, assemblyInformationalFormat);
                 var generatedAssemblyInfoPath = Path.Combine(IntermediateOutputPath, string.Format("TfsVersionTaskAssemblyInfo.g.{0}", "cs"));
                 File.WriteAllText(generatedAssemblyInfoPath, generatedAssemblyInfoText, Encoding.UTF8);
                 AssemblyInfoTempFilePath = generatedAssemblyInfoPath;
@@ -79,14 +95,23 @@ namespace SourceVersion.TfsVersionTargets
             return Path.GetDirectoryName(path);
         }
 
-        private string GenerateAssemblyInfo(int changesetNumber)
+        private string GenerateAssemblyInfo(
+            WorkspaceWatermark info,
+            string versionFormat, 
+            string assemblyInformationalFormat)
         {
+            var changeset = info.MostRecentChangeset.ToString();
+            var version = versionFormat.Replace("{changeset}", changeset);
+            var assemblyInformationalVersionSuffix = assemblyInformationalFormat
+                .Replace("{changeset}", changeset)
+                .Replace("{server-path}", info.ServerPath)
+                .Replace("{clean}", info.PendingChanges ? "Dirty" : "Clean");
+
             return
 $@"
 using System.Reflection;
-[assembly: AssemblyVersion(""1.0.1.{changesetNumber}"")]
-//[assembly: AssemblyVersion(""1.0.1.{changesetNumber}"")]
-[assembly: AssemblyInformationalVersion(""1.0.1.{changesetNumber}"")]
+[assembly: AssemblyVersion(""{version}"")]
+[assembly: AssemblyInformationalVersion(""{version}+{assemblyInformationalVersionSuffix}"")]
 ";
         }
 
